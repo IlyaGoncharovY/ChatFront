aefbab2cae2126ebd70100eec09628b92005e377
IlyaGoncharovY <goncharov.i@rambler.ru>
2024-01-08 15:25:40 +0300

    fix(cors) added cors for local dev

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
+import {io} from 'socket.io-client';
+
 function App() {
 
   const [messages, setMessages] = useState([
     {message: 'hello Vasya', id: 'e1q1q1q', user: {id: '22w2w2ww', name: 'Ilya'}},
     {message: 'hello Ilya', id: '1a1a1a', user: {id: '2s2s2s', name: 'Vasya'}},
   ]);
+
+  useEffect(() => {
+    const socket = io('http://localhost:3009');
+  }, []);
 
   return (
     <div style={{
@@ -21,7 +27,7 @@ function App() {
           width: '300px',
         }}>
           {messages.map((message) =>
-            <div>
+            <div key={message.id}>
               <b>{message.user.name}:</b> {message.message}
               <hr/>
             </div>)}
