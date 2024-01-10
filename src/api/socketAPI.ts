1343788c0c8830cdd02b70e9132df39f294abb4a
IlyaGoncharovY <goncharov.i@rambler.ru>
2024-01-10 05:16:36 +0300

    feat(api) transfer api code from new file

---
+++ src/api/socketAPI.ts
@@ -0,0 +1,41 @@
+import {io, Socket} from 'socket.io-client';
+
+import {MessageType, UserType} from '../features/chat/reducer/chatReducer.ts';
+
+//const socket = io('http://localhost:3009');
+// socket.on('init-messages-published', (messages: MessagesType[]) => {
+//     setMessages(messages);
+// });
+// socket.on('new-message-sent', (message: MessagesType) => {
+//     setMessages(prevState => [...prevState, message]);
+// });
+
+export const api = {
+  socket: null as null | Socket,
+
+  createConnection() {
+    this.socket = io('http://localhost:3009');
+  },
+  subscribe(
+    initMessagesHandler: (messages: MessageType[]) => void,
+    newMessageSentHandler: (message: MessageType) => void,
+    userTypingHandler: (user: UserType) => void,
+  ) {
+    this.socket?.on('init-messages-published', initMessagesHandler);
+    this.socket?.on('new-message-sent',newMessageSentHandler);
+    this.socket?.on('user-typing',userTypingHandler);
+  },
+  sendName (name: string) {
+    this.socket?.emit('client-name-sent', name);
+  },
+  sendMessage (message: string) {
+    this.socket?.emit('click-message-sent', message);
+  },
+  typeMessage () {
+    this.socket?.emit('client-typed');
+  },
+  destroyConnection() {
+    this.socket?.disconnect();
+    this.socket = null;
+  },
+};
