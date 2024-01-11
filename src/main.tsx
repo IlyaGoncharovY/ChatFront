c2858205e72cb55c25bbbdca2cb47ba1cce77447
IlyaGoncharovY <goncharov.i@rambler.ru>
2024-01-12 02:04:27 +0300

    fix(main) refactor import

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {Provider} from 'react-redux';
 
-import App from './features/chat/App.tsx';
+import App from './features/app/App.tsx';
 
 import './index.css';
 
-import {store} from './store/store.ts';
+import {store} from './store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
