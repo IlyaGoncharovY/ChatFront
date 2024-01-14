918da4c682ac634ae17a4c82d978578678d60728
IlyaGoncharovY <goncharov.i@rambler.ru>
2024-01-15 01:11:31 +0300

    fix(chatDisplay) added style

--- src/features/chatDisplay/ChatDisplay.tsx
+++ src/features/chatDisplay/ChatDisplay.tsx
@@ -1,12 +1,14 @@
-import {TypingUsers} from './typingUser/TypingUsers.tsx';
 import {Messages} from './messages/Messages.tsx';
+import {TypingUsers} from './typingUser/TypingUsers.tsx';
+
+import s from './ChatDisplay.module.css';
 
 export const ChatDisplay = () => {
 
   return (
-    <>
+    <div className={s.chatDisplayContainer}>
       <Messages/>
       <TypingUsers/>
-    </>
+    </div>
   );
 };
