88b505e19965a154e7510a09b494c51ae48fab70
IlyaGoncharovY <goncharov.i@rambler.ru>
2024-01-14 20:44:24 +0300

    fix(api) use .env

--- src/api/socketKey.ts
+++ src/api/socketKey.ts
@@ -1,26 +1,26 @@
 export const SOCKET_KEY = {
-    /**
+  /**
      * 'init-messages-published'
      */
-    INIT_MESSAGE: 'init-messages-published',
-    /**
+  INIT_MESSAGE: 'init-messages-published',
+  /**
      * 'new-message-sent'
      */
-    NEW_MESSAGE_SENT: 'new-message-sent',
-    /**
+  NEW_MESSAGE_SENT: 'new-message-sent',
+  /**
      * 'user-typing'
      */
-    USER_TYPING: 'user-typing',
-    /**
+  USER_TYPING: 'user-typing',
+  /**
      * 'client-name-sent'
      */
-    CLIENT_NAME: 'client-name-sent',
-    /**
+  CLIENT_NAME: 'client-name-sent',
+  /**
      * 'click-message-sent'
      */
-    MESSAGE_SENT: 'click-message-sent',
-    /**
+  MESSAGE_SENT: 'click-message-sent',
+  /**
      * 'client-typed'
      */
-    CLIENT_TYPED: 'client-typed'
-}
+  CLIENT_TYPED: 'client-typed',
+};
