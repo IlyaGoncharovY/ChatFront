14a244109b40b0247236ef6e358e4a9afd5a4a0e
IlyaGoncharovY <goncharov.i@rambler.ru>
2024-02-16 04:46:34 +0300

    feat(docker) added instructions for docker

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,26 @@
-import { defineConfig } from 'vite';
+import {defineConfig, loadEnv} from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base:'/ChatFront',
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/socket.io': {
-        target: 'http://localhost:3009',
-        changeOrigin: true,
-        ws: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const BACK_URL = env.VITE_REACT_APP_BACK_URL;
+  return {
+    base:'/ChatFront',
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/socket.io': {
+          target: 'http://localhost:3009' || BACK_URL,
+          changeOrigin: true,
+          ws: true,
+        },
       },
+      watch: {
+        usePolling: true,
+      },
+      host: true,
+      strictPort: true,
+      port: 5173,
     },
-  },
-});
+  };});
